5faecde0128f8e5dbe30a035ccb5b7a26e43c5ed
mx-in <mouxin1993@gmail.com>
2017-05-30 22:35:00 +0800

    new clearTableView

---
+++ MXDailySwift/MXDailySwift/11-ClearTableViewCell/ClearTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ClearTableViewCell.swift
+//  DailySwift
+//
+//  Created by mx_in on 2017/5/30.
+//  Copyright © 2017年 mx_in. All rights reserved.
+//
+
+import UIKit
+
+class ClearTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
