dc8e4e4aee4decf43da472621fb2cbb377034ea2
mx_in <muxin@xiaomi.com>
2017-11-15 20:30:40 +0800

    use XCode 9
    modified xcodeproj

---
+++ MXDailySwift/MXDailySwift/17-TumblrMenu/TumblrMenuTransitionManager.swift
@@ -0,0 +1,13 @@
+//
+//  MenuTransitionManager.swift
+//  DailySwift
+//
+//  Created by mx_in on 2017/11/15.
+//  Copyright © 2017年 mx_in. All rights reserved.
+//
+
+import UIKit
+
+class TumblrMenuTransitionManager: NSObject {
+
+}
