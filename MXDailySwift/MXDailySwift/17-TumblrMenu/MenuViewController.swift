76214ecec01a586b1b0d82ec459c0daff1b26d85
mx_in <muxin@xiaomi.com>
2017-11-20 16:06:12 +0800

    finish 17-TumblrMenu

--- MXDailySwift/MXDailySwift/17-TumblrMenu/MenuViewController.swift
+++ MXDailySwift/MXDailySwift/17-TumblrMenu/MenuViewController.swift
@@ -9,30 +9,36 @@
 import UIKit
 
 class MenuViewController: UIViewController {
+    
+    let transitionManager = TumblrMenuTransitionManager()
 
+    @IBOutlet weak var textButton: UIButton!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var photoButton: UIButton!
+    @IBOutlet weak var photoLabel: UILabel!
+    
+    @IBOutlet weak var quoteButton: UIButton!
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    @IBOutlet weak var linkButton: UIButton!
+    @IBOutlet weak var linkLabel: UILabel!
+    
+    @IBOutlet weak var chatButton: UIButton!
+    @IBOutlet weak var chatLabel: UILabel!
+    
+    @IBOutlet weak var audioButton: UIButton!
+    @IBOutlet weak var audioLabel: UILabel!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.transitioningDelegate = self.transitionManager;
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func didClickNeverMindBtn(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
